f6157c7de22f56a70e80dd5420f1c9ed21535ca1
Aleksandar Tepavcevic <atepavcevic29@gmail.com>
2023-12-09 03:34:04 +0100

    chore: :hammer: modify commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -34,102 +34,60 @@ module.exports = {
     ],
   },
   prompt: {
-    questions: {
-      type: {
-        description: "Select the type of change that you're committing",
-        enum: {
-          feat: {
-            description: "A new feature",
-            title: "Features",
-            emoji: "✨",
-          },
-          fix: {
-            description: "A bug fix",
-            title: "Bug Fixes",
-            emoji: "🐛",
-          },
-          docs: {
-            description: "Documentation only changes",
-            title: "Documentation",
-            emoji: "📚",
-          },
-          style: {
-            description:
-              "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
-            title: "Styles",
-            emoji: "💎",
-          },
-          refactor: {
-            description:
-              "A code change that neither fixes a bug nor adds a feature",
-            title: "Code Refactoring",
-            emoji: "📦",
-          },
-          perf: {
-            description: "A code change that improves performance",
-            title: "Performance Improvements",
-            emoji: "🚀",
-          },
-          test: {
-            description: "Adding missing tests or correcting existing tests",
-            title: "Tests",
-            emoji: "🚨",
-          },
-          build: {
-            description:
-              "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
-            title: "Builds",
-            emoji: "🛠",
-          },
-          ci: {
-            description:
-              "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
-            title: "Continuous Integrations",
-            emoji: "⚙️",
-          },
-          chore: {
-            description: "Other changes that don't modify src or test files",
-            title: "Chores",
-            emoji: "♻️",
-          },
-          revert: {
-            description: "Reverts a previous commit",
-            title: "Reverts",
-            emoji: "🗑",
-          },
-        },
+    useEmoji: true,
+    skipQuestions: ["scope", "breaking", "body"],
+    types: [
+      {
+        value: "feat",
+        name: "feat:     ✨ A new feature",
+        emoji: ":sparkles:",
       },
-      scope: {
-        description:
-          "What is the scope of this change (e.g. component or file name)",
+      { value: "fix", name: "fix:      🐛  A bug fix", emoji: ":bug:" },
+      {
+        value: "docs",
+        name: "docs:     📝  Documentation only changes",
+        emoji: ":memo:",
       },
-      subject: {
-        description:
-          "Write a short, imperative tense description of the change",
+      {
+        value: "style",
+        name: "style:    💄  Changes that do not affect the meaning of the code",
+        emoji: ":lipstick:",
       },
-      body: {
-        description: "Provide a longer description of the change",
+      {
+        value: "refactor",
+        name: "refactor: ♻️   A code change that neither fixes a bug nor adds a feature",
+        emoji: ":recycle:",
       },
-      isBreaking: {
-        description: "Are there any breaking changes?",
+      {
+        value: "perf",
+        name: "perf:     ⚡️  A code change that improves performance",
+        emoji: ":zap:",
       },
-      breakingBody: {
-        description:
-          "A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself",
+      {
+        value: "test",
+        name: "test:     ✅  Adding missing tests or correcting existing tests",
+        emoji: ":white_check_mark:",
       },
-      breaking: {
-        description: "Describe the breaking changes",
+      {
+        value: "build",
+        name: "build:    📦️   Changes that affect the build system or external dependencies",
+        emoji: ":package:",
       },
-      isIssueAffected: {
-        description: "Does this change affect any open issues?",
+      {
+        value: "ci",
+        name: "ci:       🎡  Changes to our CI configuration files and scripts",
+        emoji: ":ferris_wheel:",
       },
-      issuesBody: {
-        description:
-          "If issues are closed, the commit requires a body. Please enter a longer description of the commit itself",
+      {
+        value: "chore",
+        name: "chore:    🔨  Other changes that don't modify src or test files",
+        emoji: ":hammer:",
       },
-      issues: {
-        description: 'Add issue references (e.g. "fix #123", "re #123".)',
+      {
+        value: "revert",
+        name: "revert:   ⏪️  Reverts a previous commit",
+        emoji: ":rewind:",
       },
-    },
+    ],
   },
 };
