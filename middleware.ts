f47a68eec90fac03f92b68f7b30f8501cb170140
Aleksandar Tepavcevic <atepavcevic29@gmail.com>
2024-01-07 00:28:09 +0100

    feat: update middleware logic to support protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,36 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { createClient } from "@/lib/supabase/middleware";
+import { NextResponse, NextRequest } from 'next/server';
+import { createClient } from '@/lib/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  try {
-    // This `try/catch` block is only here for the interactive tutorial.
-    // Feel free to remove once you have Supabase connected.
     const { supabase, response } = createClient(request);
+    const isRoot = request.nextUrl.pathname === '/';
+    const isAuth =
+        request.nextUrl.pathname.startsWith('/login') ||
+        request.nextUrl.pathname.startsWith('/register');
+    const { data } = await supabase.auth.getSession();
 
-    // Refresh session if expired - required for Server Components
-    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-    await supabase.auth.getSession();
+    if (!data.session) {
+        if (isRoot || isAuth) return response;
+
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if (isAuth)
+        return NextResponse.redirect(new URL('/dashboard', request.url));
 
     return response;
-  } catch (e) {
-    // If you are here, a Supabase client could not be created!
-    // This is likely because you have not set up environment variables.
-    // Check out http://localhost:3000 for Next Steps.
-    return NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    });
-  }
 }
+
+// Ensure the middleware is only called for relevant paths.
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * Feel free to modify this pattern to include more paths.
+         */
+        '/((?!_next/static|_next/image|favicon.ico).*)',
+    ],
+};
